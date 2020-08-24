d495d7b91d6a2bb6df111435dc02c2b87218d5bc
Alfaroque Islam <alfaroque5@gmail.com>
2020-08-24 09:52:54 -0400

    initial commit

--- app/api/predict.py
+++ app/api/predict.py
@@ -3,10 +3,41 @@ import random
 
 from fastapi import APIRouter
 import pandas as pd
+import numpy as np
 from pydantic import BaseModel, Field, validator
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
 
 log = logging.getLogger(__name__)
 router = APIRouter()
+
+
+df = pd.read_csv('airbnb_BW.csv', index_col=0)
+dataset = df.values
+X = dataset[:,0:12]
+y = dataset[:,12]
+y=np.reshape(y, (-1,1))
+scaler_x = MinMaxScaler()
+scaler_y = MinMaxScaler()
+scaler_x.fit(X)
+xscale=scaler_x.transform(X)
+scaler_y.fit(y)
+yscale=scaler_y.transform(y)
+X = StandardScaler().fit_transform(X)
+y = StandardScaler().fit_transform(y.reshape(len(y),1))[:,0]
+X_train, X_test, y_train, y_test = train_test_split(xscale, yscale)
+model = Sequential()
+model.add(Dense(12, input_dim=12, kernel_initializer='normal', activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='linear'))
+model.compile(loss='mse', optimizer='adam', metrics=['mse','mae'])
+history = model.fit(X_train, y_train, epochs=150, batch_size=50,  verbose=1, validation_split=0.2)
 
 
 class Item(BaseModel):
@@ -14,13 +45,13 @@ class Item(BaseModel):
 
     zipcode: float = Field(..., example=10453)
     property_type: str = Field(..., example='House')
-    square_footage: float = Field(..., example=1000)
-    bedrooms: float = Field(..., example=2)
-    bathrooms: float = Field(..., example=1)
-    review_score_rating: float = Field(..., example=97)
-    accommodates: float = Field(..., example=4)
-    cancellation_policy: str = Field(..., example='strict')
-    cleaning_fee: float = Field(..., example=100)
+    square_footage: float = Field(..., example=4000)
+    bedrooms: float = Field(..., example=6)
+    bathrooms: float = Field(..., example=2)
+    review_score_rating: float = Field(..., example=70)
+    accommodates: float = Field(..., example=1)
+    cancellation_policy: str = Field(..., example='moderate')
+    cleaning_fee: float = Field(..., example=300)
     free_parking: str = Field(..., example='yes')
     wifi: str = Field(..., example='yes')
     cable_tv: str = Field(..., example='yes')
@@ -29,16 +60,6 @@ class Item(BaseModel):
         """Convert pydantic object to pandas dataframe with 1 row."""
         return pd.DataFrame([dict(self)])
 
-def format_input(zipcode, square_footage, bedrooms, bathrooms, review_score_rating,
-                accommodates, cleaning_fee, free_parking, wifi, cable_tv, property_type, 
-                cancellation_policy):
-                Dict = {'flexible' : 0, 'moderate' : 1, 'strict' : 2, 'yes' : 1, 'no' : 0}
-                prop_type = Dict.get(property_type)
-                can_pol = Dict.get(cancellation_policy)
-                free_park = Dict.get(free_parking)
-                wi_fi = Dict.get(wifi)
-                cab_tv = Dict.get(cable_tv)
-                return zipcode, square_footage, bedrooms, bathrooms, review_score_rating, accommodates, cleaning_fee, free_park, wi_fi, cab_tv, prop_type, can_pol
 
 
 
@@ -47,11 +68,20 @@ async def predict(item: Item):
     """Make random baseline predictions for classification problem."""
     X_new = item.to_df()
     log.info(X_new)
-    #model = model.load()
-    #y_pred = model.predict(format_input(X_new['zipcode'].iloc[0], X_new['square_footage'].iloc[0], X_new['bedrooms'].iloc[0], X_new['bathrooms'].iloc[0], 
-    #                       X_new['review_score_rating'].iloc[0], X_new['accommodates'].iloc[0], X_new['cleaning_fee'].iloc[0], X_new['free_parking'].iloc[0], 
-    #                       X_new['wifi'].iloc[0], X_new['cable_tv'].iloc[0], X_new['property_type'].iloc[0], X_new['cancellation_policy'].iloc[0],))
-    y_pred = random.randint(100, 500)
+    Dict = {'Apartment' : 1, 'House' : 0, 'flexible' : 0, 'moderate' : 1, 'strict' : 2, 'yes' : 1, 'no' : 0}
+    prop_type = Dict.get(X_new['property_type'].iloc[0])
+    can_pol = Dict.get(X_new['cancellation_policy'].iloc[0])
+    free_park = Dict.get(X_new['free_parking'].iloc[0])
+    wi_fi = Dict.get(X_new['wifi'].iloc[0])
+    cab_tv = Dict.get(X_new['cable_tv'].iloc[0])
+    Xnew = np.array([[X_new['zipcode'].iloc[0], X_new['square_footage'].iloc[0], X_new['bedrooms'].iloc[0], X_new['bathrooms'].iloc[0], 
+                           X_new['review_score_rating'].iloc[0], X_new['accommodates'].iloc[0], X_new['cleaning_fee'].iloc[0], float(free_park), 
+                           float(wi_fi), float(cab_tv), float(prop_type), float(can_pol)]])
+    Xnew= scaler_x.transform(Xnew)
+    y_pred = model.predict(Xnew)
+    y_pred = scaler_y.inverse_transform(y_pred)
+    y_pred = float(y_pred[0][0])
+    #y_pred = float(random.randint(100, 500))
     return {
         'prediction': y_pred
     }
