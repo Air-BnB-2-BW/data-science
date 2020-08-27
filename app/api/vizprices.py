8e1069fd64d7976476ea27348a73d3064a890d2b
Evan Grinalds <61983308+evangrinalds@users.noreply.github.com>
2020-08-27 11:02:16 -0700

    Update vizprices.py

--- app/api/vizprices.py
+++ app/api/vizprices.py
@@ -7,10 +7,13 @@ import plotly.graph_objects as go
 router = APIRouter()
 
 @router.get('/vizprices')
-async def visual(x,y):
+async def visual():
     # load in airbnb dataset
     DATA_PATH = 'https://raw.githubusercontent.com/Air-BnB-2-BW/data-science/master/airbnb_bw.csv'
     df = pd.read_csv(DATA_PATH, index_col=0)
+    
+    x = ['$0-25', '$25-50', '$50-75', '$75-100', '$100-125', '$125-150', '$150-175', '$175-200', '$200+']
+    y = [27, 272, 325, 125, 164, 93, 45, 22 ,13]
 
     fig = go.Figure(data=[go.Bar(x=x, y=y)])
 
