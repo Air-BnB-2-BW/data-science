f415adfa84bb031fc9db399ecf6752eab31fb29e
Alfaroque Islam <alfaroque5@gmail.com>
2020-08-26 20:16:17 -0400

    minor change to title/description

--- app/main.py
+++ app/main.py
@@ -5,8 +5,8 @@ import uvicorn
 from app.api import predict, viz
 
 app = FastAPI(
-    title='DS API',
-    description='Lorem ipsum',
+    title='AirBnB DS API',
+    description='',
     version='0.1',
     docs_url='/',
 )
